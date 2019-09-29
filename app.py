239be5878f922008f53acb570901841294cab294
sandycamilo <scam499@lasierra.edu>
2019-09-29 12:56:32 -0700

    stretch challenge

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Return homepage"""
-    return render_template('home.html', msg= 'Flask is Cool!!')
+# @app.route('/')
+# def index():
+#     """Return homepage"""
+#     # return render_template('home.html', msg= 'Flask is Cool!!')
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -14,7 +14,10 @@ if __name__ == '__main__':
 ## OUR MOCK ARRAY OF PROJECTS
 playlists = [
     { 'title': 'Cat videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
+    { 'title': '90\'s Music', 'description': 'Creep'},
+    { 'title': '60\'s Music', 'description': 'Wild Thing'},
+    { 'title': '50\'s Music', 'description': 'Dream Lover'}
 ]
 
 @app.route('/')
