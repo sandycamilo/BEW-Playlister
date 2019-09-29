d896f4283efd17a97acc8db82be35f4ae43e8845
sandycamilo <scam499@lasierra.edu>
2019-09-29 12:00:51 -0700

    sytax error line 8 app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage"""
-    return render_template('home.html'), msg= 'Flask is Cool!!')
+    return render_template('home.html', msg= 'Flask is Cool!!')
 
 if __name__ == '__main__':
     app.run(debug=True)
