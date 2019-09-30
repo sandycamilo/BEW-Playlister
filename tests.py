6b5ce2b5af36836c2e15817f67e1138e399148ae
sandycamilo <scam499@lasierra.edu>
2019-09-29 23:23:01 -0700

    added tests

--- tests.py
+++ tests.py
@@ -61,6 +61,29 @@ class PlaylistsTests(TestCase):
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'Cat Videos', result.data)
 
+    @mock.patch('pymongo.collection.Collection.insert_one')
+    def test_submit_playlist(self, mock_insert):
+        """Test submitting a new playlist."""
+        result = self.client.post('/playlists', data=sample_form_data)
+
+        ## After submitting, should redirect to that playlist's page
+        self.assertEqual(result.status, '302 FOUND')
+        mock_insert.assert_called_with(sample_playlist)
+
+    @mock.patch('pymongo.collection.Collection.update_one')
+    def test_update_playlist(self, mock_update):
+        result = self.client.post(f'/playlists/{sample_playlist_id}', data=sample_form_data)
+        
+        self.assertEqual(result.status, '302 FOUND')
+        mock_update.assert_called_with({'_id': sample_playlist_id}, {'$set': sample_playlist})
+
+    @mock.patch('pymongo.collection.Collection.delete_one')
+    def test_delete_playlist(self,mock_delete):
+        form_data = {'_method': 'DELETE'}
+        result = self.client.post(f'/playlists/{sample_playlist_id}/delete', data= form_data)
+        self.assertEqual(result.status, '302 FOUND')
+        mock_delete.assert_called_with({'_id: sample_playlist_id'})
+
 
 if __name__ == '__main__':
     unittest_main()
